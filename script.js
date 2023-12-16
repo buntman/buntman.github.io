7f2f260ddc5e632780b4f0b45224e8d997aebc89
buntman <ranmiem@gmail.com>
2023-12-16 23:02:28 +0800

    Shop first commit

--- script.js
+++ script.js
@@ -14,9 +14,68 @@ $(document).ready(function(){
     });
   });
 
+// HAMBURGER MENU
   $(document).ready(function() {
     $(".burger-menu").click(function() {
         $(this).toggleClass("fa-times");
         $(".navigation-menu").toggleClass("active");
     });
+});
+
+// SHOP IMAGE HOVER EFFECTS
+$(document).ready(function(){
+  $('.shop-image .thumbnail').hover(function(){
+    $(this).css('transform', 'scale(1.1)'); 
+  }, function(){
+    $(this).css('transform', 'scale(1)'); 
+  });
+}); 
+
+// HOME THUMBNAIL HOVER EFFECTS
+$(document).ready(function(){
+  $('.thumbnail-holder').hover(function(){
+    $(this).css('transform', 'scale(1.05)'); 
+  }, function(){
+    $(this).css('transform', 'scale(1)'); 
+  });
+}); 
+
+
+// SHOP'S SIDEBAR
+$(document).ready(function() {
+  var menuOpen = false;
+  $('#sidebar-btn').on('click', function() {
+    $('#sidebar, #sidebar-btn').toggleClass('visible');
+    if (!menuOpen) {
+      $(this).addClass('open');
+      menuOpen = true;
+    } else {
+      $(this).removeClass('open');
+      menuOpen = false;
+    }
+    
+    $('.overlay').toggle();
+    
+    if ($('#sidebar').hasClass('visible')) {
+      $('body').css('overflow', 'hidden');
+    } else {
+      $('body').css('overflow', 'auto');
+    }
+  });
+  $('#sidebar-btn.visible').on('click', function() {
+    $('body').css('overflow', 'auto');
+  });
+  var x = window.matchMedia("(max-width: 992px)");
+
+  function myFunction(x) {
+    if (x.matches) {
+      $('.filter').attr("id", "sidebar")
+    } else {
+      $("#sidebar").removeAttr("id");
+    }
+
+  }
+  x.addListener(function() {
+    myFunction(x);
+  });
 });
