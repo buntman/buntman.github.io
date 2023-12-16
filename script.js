384f57fecfc80c63b3c7ea6b7a6300cdc51d98b7
buntman <ranmiem@gmail.com>
2023-12-16 11:45:55 +0800

    Small changes

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 $(document).ready(function(){
     $('.third-content img').hover(function(){
-      $(this).css('transform', 'scale(1.2)'); // Scale to 1.2 times the original size
+      $(this).css('transform', 'scale(1.2)'); 
     }, function(){
-      $(this).css('transform', 'scale(1)'); // Scale back to the original size
+      $(this).css('transform', 'scale(1)'); 
     });
   });
   
   $(document).ready(function(){
     $('.second-content img').hover(function(){
-      $(this).css('opacity', '0.9'); // Or toggle a class for overlay effects
+      $(this).css('opacity', '0.9'); //overlay effects
     }, function(){
       $(this).css('opacity', '1');
     });
